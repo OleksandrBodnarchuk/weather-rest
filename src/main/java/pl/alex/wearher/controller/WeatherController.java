0e0998e73d5f660a2075c185573862793d83d8eb
OleksandrBodnarchuk <obodnarchuk@outlook.com>
2022-01-18 17:24:30 +0100

    GET method + sample getWeather() in Service class

--- src/main/java/pl/alex/wearher/controller/WeatherController.java
+++ src/main/java/pl/alex/wearher/controller/WeatherController.java
@@ -1,4 +1,19 @@
 package pl.alex.wearher.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.alex.wearher.model.WeatherDTO;
+import pl.alex.wearher.service.WeatherService;
+
+@RestController
+@AllArgsConstructor
 public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    @GetMapping("/weather")
+    public WeatherDTO getWeather() {
+        return weatherService.getWeather();
+    }
 }
--- src/main/java/pl/alex/wearher/service/WeatherService.java
+++ src/main/java/pl/alex/wearher/service/WeatherService.java
@@ -1,4 +1,12 @@
 package pl.alex.wearher.service;
 
+import org.springframework.stereotype.Service;
+import pl.alex.wearher.model.WeatherDTO;
+
+@Service
 public class WeatherService {
+
+    public WeatherDTO getWeather(){
+        return null;
+    }
 }
